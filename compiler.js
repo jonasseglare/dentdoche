8d4622a33a46082cc42479b5fa813529c7c8b070
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-05-24 20:20:48 +0200

    added some files

--- compiler.js
+++ compiler.js
@@ -1,1 +1,8 @@
 var common = require('./common');
+
+function isCompiled(x) {
+  if (typeof x == 'function') {
+    return x.compiled;
+  }
+  return false;
+}
---
+++ test/compiler.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var immutable = require('immutable');
+var fs = require('fs');
+var c = require('../compiler.js');
+
+describe('compilers', function() {
+  it('Test isCompiled', function() {
+
+  });
+});
