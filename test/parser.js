1c1ecef5911added9fe04933454655f95d4b3122
Jonas Östlund <uppfinnarjonas@gmail.com>
2015-05-27 16:29:03 +0200

    added test for eval

---
+++ test/parser.js
@@ -0,0 +1,15 @@
+// S-expr to array parser: https://www.npmjs.com/package/sexp
+// https://www.npmjs.com/package/sexp-tokenizer
+//
+// Example usage:
+// eval(dd.parse('(var fs (require "fs"))'));
+// eval(dd.parse('(defn abra [x] (+ 1 2 x))'));
+
+var assert = require('assert');
+
+describe('Trying eval', function() {
+  it('Should load fs using eval', function() {
+    eval('var fs = require("fs"); var abc = fs.readFile');
+    assert(typeof abc == 'function');
+  });
+});
